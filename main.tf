d85a353c19eb08340127ce1c87e053fbdf049faf
Devcypher2023 <charleston5000@yahoo.com>
2024-09-23 20:43:16 -0500

    update

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ resource "aws_instance" "server1" {
   ami           = data.aws_ami.ami1.id
 
   tags ={
-    Name = "Charleston"
+    Name = "Devops"
+  }
+}
+
+resource "aws_instance" "server2" {
+  instance_type = var.instance-type
+  ami           = data.aws_ami.ami1.id
+
+  tags ={
+    Name = "Terraform-up"
   }
 }
